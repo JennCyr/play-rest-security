6c72dbebbe0a1afe48b18ec25fbf4a3ac4998437
JennCyr <eric_and_jenn@hotmail.com>
2016-04-29 10:23:01 -0300

    Update TodoController.java

--- app/controllers/TodoController.java
+++ app/controllers/TodoController.java
@@ -15,10 +15,37 @@ public class TodoController extends Controller {
 
     @Inject FormFactory formFactory;
 
+@ApiOperation(value = "get All Todos",
+     notes = "Returns List of all Todos",
+     response = Todo.class, 
+     httpMethod = "GET") 
     public Result getAllTodos() {
         return ok(toJson(Todo.findByUser(SecurityController.getUser())));
     }
 
+@ApiOperation( 
+     nickname = "createTodo", 
+     value = "Create Todo", 
+     notes = "Create Todo record", 
+     httpMethod = "POST", 
+     response = Todo.class
+ ) 
+@ApiImplicitParams( 
+     { 
+          @ApiImplicitParam( 
+               name = "body", 
+               dataType = "Todo", 
+               required = true, 
+               paramType = "body", 
+               value = "Todo" 
+          ) 
+     } 
+) 
+@ApiResponses( 
+          value = { 
+                  @com.wordnik.swagger.annotations.ApiResponse(code = 400, message = "Json Processing Exception") 
+          } 
+) 
     public Result createTodo() {
         Form<Todo> form = formFactory.form(Todo.class).bindFromRequest();
         if (form.hasErrors()) {
